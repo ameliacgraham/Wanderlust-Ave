4c400bb49bc834bbd5a2b8c109991d83e1056393
ameliacgreen <ameliacgreen@gmail.com>
2017-03-27 06:39:34 +0000

    Added test for homepage

---
+++ tests.py
@@ -0,0 +1,39 @@
+import unittest
+
+from server import app
+from model import db, example_data, connect_to_db
+
+
+class BucketTests(unittest.TestCase):
+    """Tests for my party site."""
+
+    def setUp(self):
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+    def test_homepage(self):
+        result = self.client.get("/")
+        self.assertIn("Login", result.data)
+
+
+    """Flask tests that use the database."""
+
+    def setUp(self):
+        """Stuff to do before every test."""
+
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+        # Connect to test database (uncomment when testing database)
+        connect_to_db(app, "postgresql:///testdb")
+
+        # Create tables and add sample data (uncomment when testing database)
+        db.create_all()
+        example_data()
+
+    def tearDown(self):
+        """Do at end of every test."""
+
+        # (uncomment when testing database)
+        db.session.close()
+        db.drop_all()
